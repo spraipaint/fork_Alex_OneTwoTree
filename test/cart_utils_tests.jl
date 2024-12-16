77db5c07f93a3a97678a20b178267049eee74354
Brunololos <70522244+Brunololos@users.noreply.github.com>
2024-12-16 17:58:26 +0100

    Added subset mean tests (#29)

--- test/cart_utils_tests.jl
+++ test/cart_utils_tests.jl
@@ -16,16 +16,22 @@ using OneTwoTree
 
     combs1 = [66.41, -49.67, -87.17, -9.32, 88.04, 39.97, -76.66, 85.86, -30.45, 13.56, -51.52]
 
-    @testset "Subset Splitting" begin
+    inds1 = [1, 2, 3, 4, 5]
+    inds2 = [2, 4, 5]
+    inds3 = [5, 3, 1, 2]
+    inds4 = [2, 6, 4, 5, 7]
+    inds5 = [11, 3, 8, 9, 4, 1, 2]
 
+    @testset "Subset Splitting" begin
+        # TODO: Test split_indices
     end
 
     @testset "Class Frequency" begin
-
+        # TODO: Test most_frequent_class
     end
 
     @testset "Class Collection" begin
-
+        # TODO: Test collect_classes
     end
 
     @testset "Means" begin
@@ -33,16 +39,32 @@ using OneTwoTree
             @test label_mean(nums1) == 58.4
             @test label_mean(nums2) == 50.0
             @test isapprox(label_mean(nums3), 48.818181818182)
+
             @test label_mean(negs1) == -10.4
             @test isapprox(label_mean(negs2), -14.571428571429)
             @test isapprox(label_mean(negs3), -3.2727272727273)
+
             @test label_mean(fracs1) == 51.132
             @test isapprox(label_mean(fracs2), 54.327142857143)
             @test isapprox(label_mean(fracs3), 62.204545454545)
+
             @test isapprox(label_mean(combs1), -0.99545454545455)
         end
 
         @testset "Subset Mean" begin
+            @test label_mean(nums1, inds3) == 64.75
+            @test label_mean(nums2, inds4) == 56.8
+            @test label_mean(nums3, inds5) == 52.0
+
+            @test label_mean(negs1, inds3) == -15.75
+            @test label_mean(negs2, inds4) == -15.4
+            @test isapprox(label_mean(negs3, inds5), -6.1428571428571)
+
+            @test label_mean(fracs1, inds3) == 44.6
+            @test isapprox(label_mean(fracs2, inds4), 55.828)
+            @test isapprox(label_mean(fracs3, inds5), 56.464285714286)
+
+            @test isapprox(label_mean(combs1, inds5), -10.837142857143)
         end
     end
 end
