a968e086a52fdacedad06a6ce8bee039af4fbcd1
Jakob <jakob.balasus@gmail.com>
2025-01-13 09:55:42 +0100

    ForestRegressor added

--- src/Forest.jl
+++ src/Forest.jl
@@ -1,7 +1,10 @@
+# A forest is a collection of trees wich aggregate their decisions
+#first i cover Forests for Classification and after that Forests for regression
+
 using StatsBase: mode #TODO richtig einbinden oder selbst schreiben
 
 mutable struct ForestClassifier <: AbstractDecisionTree
-    trees::Vector{AbstractDecisionTree}
+    trees::Vector{AbstractDecisionTree} #TODO better DecisionTreeClassifier?
     n_trees::Int
     n_features_per_tree::Int
     max_depth::Int
@@ -56,11 +59,52 @@ function predict2(forest::ForestClassifier, X::Union{Matrix{S}, Vector{S}}) wher
     return mode(predictions)
 end
 
-#returns the most common value in a (predictions) vector for classification or the mean for regression 
-function voting(vec::Vector{T}) where T
-    if is_regression
-        return mean(vec)
-    else
-        return mode(vec)
+#----------------------------------------------------------------------------------------------------
+#now regression forests
+
+mutable struct ForestRegressor <: AbstractDecisionTree
+    trees::Vector{AbstractDecisionTree} #TODO better DecisionTreeRegressor?
+    n_trees::Int
+    n_features_per_tree::Int
+    max_depth::Int
+end
+
+function ForestRegressor(n_trees::Int, n_features_per_tree::Int, max_depth::Int)
+    if n_trees <= 0
+        error("A RandomForest needs more than 0 trees.\n (Currently n_trees == $n_trees)")
     end
+
+    if n_features_per_tree <= 0
+        error("A RandomForest needs more than 0 features per Tree.\n (Currently n_features_per_tree == $n_features_per_tree)")
+    end
+
+    if max_depth <= 0
+        error("A RandomForest needs more than 0 max depth per Tree.\n (Currently max_depth == $max_depth)")
+    end
+
+    ForestRegressor(Vector{AbstractDecisionTree}(), n_trees, n_features_per_tree, max_depth)
+end
+
+#function fit2!(forest::ForestClassifier, features::Matrix{Union{Real, String}}, labels::Vector{Union{Real, String}})
+function fit2!(forest::ForestRegressor, features::Matrix{S}, labels::Vector{T}, column_data=false) where {S<:Union{Real, String}, T<:Union{Number, String}}
+    for i in 1:forest.n_trees
+        println("Tree nr $i wird gefitted")
+        # get random dataset of size forest.n_features_per_tree
+        current_tree_features, current_tree_labels = get_random_features(features, labels, forest.n_features_per_tree)
+
+        # TODO Regression or Classification?
+        tree = DecisionTreeRegressor(max_depth=forest.max_depth)
+
+        fit!(tree,current_tree_features, current_tree_labels)
+        push!(forest.trees, tree)
+    end
+end
+
+#TODO rename fit2, predict2 etc
+function predict2(forest::ForestRegressor, X::Union{Matrix{S}, Vector{S}}) where S<:Union{Real, String}
+    if isempty(forest.trees)
+        error("Prediction failed because there are no trees. (Maybe you forgot to fit?)")
+    end
+    predictions = [predict(tree, X) for tree in forest.trees]
+    return mean(predictions)
 end
